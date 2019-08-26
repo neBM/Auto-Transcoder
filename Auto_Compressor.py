9270d8c8e3541d648d24ce3de2fb98cab8b7a0a7
Ben Martin <benrussellmartin@gmail.com>
2019-08-26 16:19:25 +0100

    Save args to a todo array Keep number of active transcodes at target until todo is empty Add nonce to tmp filename Fix missing log level output check Move post process file management to deticated function Move loop controller code to main call stack

--- Auto_Compressor.py
+++ Auto_Compressor.py
@@ -1,13 +1,24 @@
 import os, time, re, subprocess, sys
 from copy import deepcopy
 
-
 pathToWatch = "../media/Movies"
 #pathToExport = pathToWatch
 pathToExport = "../media/Movies"
 #pathToExport = "../torrent/compressed/movies"
 pathToMvOld = False
 before = []
+appNonce = hex(int(time.time())).split("x")[1]
+nextTranscodeId = 0
+compressionsToComplete = []
+compressions = []
+
+class transcoderController():
+    process = None
+    properties = {}
+    def start(self):
+        print(" ".join(self.properties["args"]))
+        process = subprocess.Popen(self.properties["args"])
+        return True
 
 def getMediaList(regex, prevBefore):
     files = []
@@ -40,7 +51,9 @@ def initFFMPEG(inputFileDir):
     if pathToTmp != False:
         if not os.path.exists(pathToTmp):
             os.makedirs(pathToTmp)
-        tmpAbsDir = os.path.abspath(pathToTmp + "/" + hex(int(time.time())).split("x")[1] + ".mkv")
+        tmpAbsDir = os.path.abspath(pathToTmp + "/" + appNonce + "_" + hex(nextTranscodeId).split("x")[1] + ".mkv")
+    else:
+        tmpAbsDir = False
     relativePath = pathToExport + relativeDir + "/" + pathName[0] + ".mkv"
     exportDir = os.path.abspath(relativePath)
 
@@ -52,7 +65,7 @@ def initFFMPEG(inputFileDir):
         2: "warning",
         3: "info",
         4: "verbose"
-        }
+    }
 
     args.extend(["-loglevel", "level+" + switcher[level]])
      
@@ -62,66 +75,90 @@ def initFFMPEG(inputFileDir):
     args.append(exportDir if pathToTmp == False else tmpAbsDir)
 
     if pathToMvOld != False:
-        relMvOldPath = pathToMvOld + relativeDir + "/" + os.path.basename(inputFileDir) if pathToMvOld != False else False
+        relMvOldPath = pathToMvOld + relativeDir + "/" + os.path.basename(inputFileDir)
         if level >= 4: print("Move old: " + os.path.abspath(relMvOldPath))
+    else:
+        relMvOldPath = False
 
     if level >= 4: print("Input: " + inputFileAbsDir)
     if level >= 4: print("Output: " + exportDir)
     if autoYes != True and input("Start? (y/n) ") != "y":
-        return False
+        exit()
 
     if not os.path.exists(os.path.dirname(pathToExport + relativeDir)):
         os.makedirs(os.path.dirname(pathToExport + relativeDir))
 
-    inputFileInfo = bytes(subprocess.check_output(["ffmpeg/ffprobe", "-of", "csv", "-v", "error", "-show_streams", inputFileAbsDir])).decode(sys.stdout.encoding)
+    if "runRules" not in os.environ or os.environ["runRules"] == "True":
+        inputFileInfo = bytes(subprocess.check_output(["ffmpeg/ffprobe", "-of", "csv", "-v", "error", "-show_streams", inputFileAbsDir])).decode(sys.stdout.encoding)
 
-    streams = [stream.split(",") for stream in inputFileInfo.split("\n")]
-    streams.remove([""])
+        streams = [stream.split(",") for stream in inputFileInfo.split("\n")]
+        streams.remove([""])
 
-    videoStreams = []
-    audioStreams = []
-    otherStreams = []
-    for stream in streams:
-        if stream[0] != "stream":
-            continue
-        if stream[5] == "video":
-            videoStreams.append(stream)
-        elif stream[5] == "audio":
-            audioStreams.append(stream)
-        else:
-            otherStreams.append(stream)
+        videoStreams = []
+        audioStreams = []
+        otherStreams = []
+        for stream in streams:
+            if stream[0] != "stream":
+                continue
+            if stream[5] == "video":
+                videoStreams.append(stream)
+            elif stream[5] == "audio":
+                audioStreams.append(stream)
+            else:
+                otherStreams.append(stream)
 
-    needsCompression = False
-    for stream in videoStreams:
-        if stream[2] != "hevc" or stream[10] > resCap:
-            needsCompression = True
-            break
+        needsCompression = False
+        for stream in videoStreams:
+            if stream[2] != "hevc" or stream[10] > resCap:
+                needsCompression = True
+                break
+    else:
+        needsCompression = True
+
+    properties = {
+        "args": args,
+        "postProcessProperties": {
+            "inputFileDir": inputFileDir,
+            "relMvOldPath": relMvOldPath,
+            "inputFileAbsDir": inputFileAbsDir,
+            "relativeDir": relativeDir,
+            "relativePath": relativePath,
+            "pathName": pathName,
+            "exportDir": exportDir,
+            "tmpAbsDir": tmpAbsDir,
+            "pathName": pathName,
+            "neededCompression": needsCompression
+        }
+    }
 
     if needsCompression == True:
-        subprocess.Popen(args).wait()
-
-    if pathToMvOld != False:
-        if not os.path.exists(os.path.dirname(relMvOldPath)):
-            os.makedirs(os.path.dirname(relMvOldPath))
-        os.rename(inputFileDir, relMvOldPath)
-
-    if needsCompression == False:
-        if level >= 2: print("Compression not needed for " + inputFileAbsDir)
-        relativePath = pathToExport + relativeDir + "/" + pathName[0] + pathName[1]
-        if not os.path.exists(os.path.dirname(relativePath)):
-            os.makedirs(os.path.dirname(relativePath))
-        os.link(relativePath if pathToMvOld == False else relMvOldPath, inputFileDir)
-    elif pathToTmp != False:
-        if not os.path.exists(os.path.dirname(exportDir)):
-            os.makedirs(os.path.dirname(exportDir))
-        os.rename(tmpAbsDir, exportDir)
-
-    if pathToMvOld != False:
-        addProcessed(inputFileDir, relativePath, relMvOldPath)
+        return properties
     else:
-        addProcessed(inputFileDir, relativePath)
+        processOutput(properties["postProcessProperties"])
 
     return True
+
+def processOutput(postProcessProperties):
+    if pathToMvOld != False:
+        if not os.path.exists(os.path.dirname(postProcessProperties["relMvOldPath"])):
+            os.makedirs(os.path.dirname(postProcessProperties["relMvOldPath"]))
+        os.rename(postProcessProperties["inputFileDir"], postProcessProperties["relMvOldPath"])
+
+    if postProcessProperties["neededCompression"] == False:
+        if level >= 2: print("Compression not needed for " + postProcessProperties["inputFileAbsDir"])
+        postProcessProperties["relativePath"] = pathToExport + postProcessProperties["relativeDir"] + "/" + postProcessProperties["pathName"][0] + postProcessProperties["pathName"][1]
+        if not os.path.exists(os.path.dirname(postProcessProperties["relativePath"])):
+            os.makedirs(os.path.dirname(postProcessProperties["relativePath"]))
+        os.link(postProcessProperties["relativePath"] if pathToMvOld == False else postProcessProperties["relMvOldPath"], postProcessProperties["inputFileDir"])
+    elif pathToTmp != False:
+        if not os.path.exists(os.path.dirname(postProcessProperties["exportDir"])):
+            os.makedirs(os.path.dirname(postProcessProperties["exportDir"]))
+        os.rename(postProcessProperties["tmpAbsDir"], postProcessProperties["exportDir"])
+
+    if pathToMvOld != False:
+        addProcessed(postProcessProperties["inputFileDir"], postProcessProperties["relativePath"], postProcessProperties["relMvOldPath"])
+    else:
+        addProcessed(postProcessProperties["inputFileDir"], postProcessProperties["relativePath"])
 
 def addProcessed(relInputPath, relOutputPath, relMvOldPath=None):
     if relMvOldPath != None and pathToMvOld != False and os.path.commonpath([pathToMvOld, pathToWatch]) == os.path.relpath(pathToWatch):
@@ -142,20 +179,6 @@ def addToProcessedFile(fileDir):
         processedFile.write("\t" + fileDir)
     processed.append(processedFile)
 
-
-def iterate():
-    while True:
-        media = getMediaList(re.compile(r"(.webm|.mkv|.flv|.flv|.vob|.ogv|.ogg|.drc|.gif|.gifv|.mng|.avi|.MTS|.M2TS|.TS|.mov|.qt|.wmv|.yuv|.rm|.rmvb|.asf|.amv|.mp4|.m4p|.m4v|.mpg|.mp2|.mpeg|.mpe|.mpv|.mpg|.mpeg|.m2v|.m4v|.svi|.3gp|.3g2|.mxf|.roq|.nsv|.flv|.f4v|.f4p|.f4a|.f4b)$", flags=re.IGNORECASE), before)
-        for x in media:
-            try:
-                if not initFFMPEG(x): exit()
-            except subprocess.CalledProcessError:
-                if level >= 1: print("Transcoder Error! Failed to process: " + x)
-                os.remove(x)
-        if loop == False:
-            exit()
-        time.sleep(loop * 60)
-
 autoYes = os.environ["autoYes"] == "True"
 loop = int(os.environ["loop"]) if "loop" in os.environ.keys() and os.environ["loop"] != "False" else False
 if "watch" in os.environ.keys():
@@ -169,6 +192,7 @@ pathToMvOld = os.environ["mvold"] if "mvold" in os.environ.keys() and os.environ
 pathToTmp = os.environ["tmp"] if "tmp" in os.environ.keys() and os.environ["tmp"] != "False" else False
 resCap = os.environ["rescap"] if "rescap" in os.environ.keys() and os.environ["rescap"] != "False" else False
 level = os.environ["level"] if "level" in os.environ.keys() else "warning"
+concurrentCompressionsTarget = 3
 
 class levels:
     disabled = 0
@@ -191,4 +215,28 @@ else:
     os.environ["processed"] = "False"
     processed = []
 
-iterate()
+while True:
+    media = getMediaList(re.compile(r"(.webm|.mkv|.flv|.flv|.vob|.ogv|.ogg|.drc|.gif|.gifv|.mng|.avi|.MTS|.M2TS|.TS|.mov|.qt|.wmv|.yuv|.rm|.rmvb|.asf|.amv|.mp4|.m4p|.m4v|.mpg|.mp2|.mpeg|.mpe|.mpv|.mpg|.mpeg|.m2v|.m4v|.svi|.3gp|.3g2|.mxf|.roq|.nsv|.flv|.f4v|.f4p|.f4a|.f4b)$", flags=re.IGNORECASE), before)
+    for x in media:
+        try:
+            properties = initFFMPEG(x)
+            nextTranscodeId += 1
+            if properties != False:
+                compressionsToComplete.append(properties)
+        except subprocess.CalledProcessError:
+            if level >= 1: print("Transcoder Error! Failed to process: " + x)
+            os.remove(x)
+    while len(compressionsToComplete) > 0:
+        for compression in compressions:
+            if compression.process != None and compression.process.poll() == 0:
+                compressions.remove(compression)
+                processOutput(compression.properties["postProcessProperties"])
+        while len(compressions) < concurrentCompressionsTarget and len(compressionsToComplete) > 0:
+            newTranscoderController = transcoderController()
+            newTranscoderController.properties = compressionsToComplete.pop(0)
+            newTranscoderController.start()
+            compressions.append(newTranscoderController)
+        time.sleep(10)
+    if loop == False:
+        exit()
+    time.sleep(loop * 60)
